308fdae060779294f3be03c221c3bbcd4761d2c9
John Parker <jparker@urgetopunt.com>
2014-04-03 11:12:52 -0700

    flash.each yields severity as String?

--- templates/flash_helper.rb
+++ templates/flash_helper.rb
@@ -5,7 +5,7 @@ module FlashHelper
   }.tap { |h| h.default = 'alert alert-info' }
 
   def flash_div(severity, &block)
-    content_tag :div, class: SEVERITIES[severity], data: {dismiss: 'alert'} do
+    content_tag :div, class: SEVERITIES[severity.to_sym], data: {dismiss: 'alert'} do
       content_tag :p, class: 'lead' do
         content_tag(:span, raw('&times;'), class: 'close') + capture(&block)
       end
