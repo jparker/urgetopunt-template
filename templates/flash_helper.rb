218e06fff9a2969263efd0e5f9b690497a1f119f
John Parker <jparker@urgetopunt.com>
2014-04-03 11:10:06 -0700

    missing &block in #flash_div

--- templates/flash_helper.rb
+++ templates/flash_helper.rb
@@ -4,7 +4,7 @@ module FlashHelper
     notice: 'alert alert-success',
   }.tap { |h| h.default = 'alert alert-info' }
 
-  def flash_div(severity)
+  def flash_div(severity, &block)
     content_tag :div, class: SEVERITIES[severity], data: {dismiss: 'alert'} do
       content_tag :p, class: 'lead' do
         content_tag(:span, raw('&times;'), class: 'close') + capture(&block)
