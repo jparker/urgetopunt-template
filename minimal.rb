3a1446f7a4b8a61d8afaac5faa7df249959171db
John Parker <jparker@urgetopunt.com>
2015-03-15 16:28:32 -0700

    always use SQL as schema format

--- minimal.rb
+++ minimal.rb
@@ -4,6 +4,9 @@ inject_into_file 'Gemfile', "ruby '#{RUBY_VERSION}'\n", after: /\Asource .*\n/
 
 gem 'pry-rails'
 
+# maintain database schema in sql format to support triggers, etc.
+application "config.active_record.schema_format = :sql\n"
+
 bundle_opts = ENV.fetch('BUNDLE_OPTS') { '-j2' }
 run "bundle install #{bundle_opts}"
 run 'bundle exec spring binstub --all'
--- postgresql.rb
+++ postgresql.rb
@@ -1,3 +1,6 @@
 # install migration to create citext extension
 template 'create_citext_extension.rb',
   'db/migrate/001_create_citext_extension.rb'
+
+# maintain database schema in sql format to support triggers, etc.
+application "config.active_record.schema_format = :sql\n"
