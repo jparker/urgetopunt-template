df9e76df8c76a3a21e4f06680913ada2f10d3824
John Parker <jparker@urgetopunt.com>
2015-05-23 00:23:09 -0700

    add test#exception to test exception notification

--- exceptions.rb
+++ exceptions.rb
@@ -1,2 +1,9 @@
 generate 'exception_notification:install'
-@todo << 'Update config/initializers/exception_notification.rb'
+
+route "get 'test/exception' => 'test#exception'"
+generate 'controller', 'test'
+inject_into_file 'app/controllers/test_controller.rb', before: /end/ do
+  "  def exception; raise 'test exception notification'; end\n"
+end
+
+@todo << 'Update config/initializers/exception_notification.rb and GET /test/exception'
