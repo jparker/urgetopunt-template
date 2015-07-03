ff0878a946b7dbca2024bb30fe10b08524aea61c
John Parker <jparker@urgetopunt.com>
2015-07-03 02:03:11 -0700

    renamed exception testing controller

--- exceptions.rb
+++ exceptions.rb
@@ -1,9 +1,9 @@
 generate 'exception_notification:install'
 
-route "get 'test/exception' => 'test#exception'"
-generate 'controller', 'test'
-inject_into_file 'app/controllers/test_controller.rb', before: /end/ do
-  "  def exception; raise 'test exception notification'; end\n"
+generate 'controller', 'exception'
+route "get 'exception/test' => 'exception#test'"
+inject_into_file 'app/controllers/exception_controller.rb', before: /end/ do
+  "  def test; raise 'test exception notification'; end\n"
 end
 
 @todo << 'Update config/initializers/exception_notification.rb and perform a GET /test/exception'
