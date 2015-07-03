46bc618557dbb00c076f4a14412e4a6cf0f7ff24
John Parker <jparker@urgetopunt.com>
2015-07-02 18:30:08 -0700

    tweak todo message

--- exceptions.rb
+++ exceptions.rb
@@ -6,4 +6,4 @@ inject_into_file 'app/controllers/test_controller.rb', before: /end/ do
   "  def exception; raise 'test exception notification'; end\n"
 end
 
-@todo << 'Update config/initializers/exception_notification.rb and GET /test/exception'
+@todo << 'Update config/initializers/exception_notification.rb and perform a GET /test/exception'
