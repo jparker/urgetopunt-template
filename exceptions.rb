4492918cbbc15da267e846e69edb00d90de1f23e
John Parker <jparker@urgetopunt.com>
2016-02-24 16:16:05 -0800

    give ExcpetionController#test a better explanation

--- exceptions.rb
+++ exceptions.rb
@@ -3,7 +3,12 @@ generate 'exception_notification:install'
 generate 'controller', 'exception'
 route "get 'exception/test' => 'exception#test'"
 inject_into_file 'app/controllers/exception_controller.rb', before: /end/ do
-  "  def test; raise 'test exception notification'; end\n"
+  <<-RUBY
+  def test
+    raise 'This action deliberately raises a RuntimeError' \
+      ' in order to test exception notification.'
+  end
+  RUBY
 end
 
 @todo << 'Update config/initializers/exception_notification.rb and perform a GET /test/exception'
