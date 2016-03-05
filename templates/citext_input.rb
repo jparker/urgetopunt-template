174edf97c475209ef62a9504e5df10a054d33a39
John Parker <jparker@urgetopunt.com>
2016-03-05 01:26:42 -0800

    updated citext input to generate input[type="text"] instead of type="citext"

--- templates/citext_input.rb
+++ templates/citext_input.rb
@@ -1,3 +1,7 @@
 # Placeholder class to tell SimpleForm to treat citext columns as strings
 class CitextInput < SimpleForm::Inputs::StringInput
+  def input(wrapper_options)
+    input_html_classes << 'form-control' << 'string'
+    @builder.text_field attribute_name, input_html_options
+  end
 end
