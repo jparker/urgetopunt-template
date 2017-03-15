f5fff04263ef547ed963a80c4832f28b42d8fb42
John Parker <jparker@urgetopunt.com>
2017-03-15 13:51:04 -0700

    customize pry prompt based on environment

--- base_pre.rb
+++ base_pre.rb
@@ -2,11 +2,10 @@
 inject_into_file 'Gemfile', "ruby '#{RUBY_VERSION}'\n", after: /\Asource .*\n/
 create_file '.ruby-version', "#{RUBY_VERSION}\n"
 
-# housekeeping that must be performed before anything below
-create_file '.env'
+create_file '.env', "SENSIBLE_DEFAULTS=true\n"
+create_file '.env.development', "HEROKU_APP_NAME=#{@app_name}-development\n"
+create_file '.env.test', "HEROKU_APP_NAME=#{@app_name}-test\n"
 create_file 'Procfile'
-
-append_file '.env', "SENSIBLE_DEFAULTS=true\n"
 
 # maintain database schema in sql format to support triggers, etc.
 application "config.active_record.schema_format = :sql\n"
--- console.rb
+++ console.rb
@@ -1,1 +1,30 @@
-create_file '.pryrc', "AwesomePrint.pry!\n"
+create_file '.pryrc', <<-RUBY
+AwesomePrint.pry!
+
+if defined?(Rails)
+  Pry.config.prompt = lambda do |obj, nest_level, _|
+    "\#{app_name}/\#{env_name} \#{obj}:\#{nest_level}> "
+  end
+end
+
+def app_name
+  ENV['HEROKU_APP_NAME'][/\A(.*)-/, 1]
+end
+
+def env_name
+  ENV['HEROKU_APP_NAME'].match /-([[:alpha:]]+)\z/ do |m|
+    case m[1]
+    when 'development'
+      Pry::Helpers::Text.bright_green m[1]
+    when 'test'
+      Pry::Helpers::Text.bright_blue m[1]
+    when 'staging'
+      Pry::Helpers::Text.bright_yellow m[1]
+    when 'production'
+      Pry::Helpers::Text.bright_red m[1]
+    else
+      Pry::Helpers::Text.bright_default m[1]
+    end
+  end
+end
+RUBY
